20c245d9fe169c1070727d979fa480c61e432b2c
ameer <ameer.em.89@gmail.com>
2022-07-15 22:17:06 +0300

    feat: making some changes in cart.js file and update changes in css file

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,37 +1,33 @@
 import React from "react";
 import "./Cart.css";
+
 function Cart({ cart }) {
+  const tablerows = cart.map((shoe, i) => {
+    return (
+      <tr key={i} className="shoes-row">
+        <td>{shoe.name}</td>
+        <td>{shoe.size}</td>
+        <td>{shoe.price}</td>
+        <td>
+          <img src={shoe.imgUrl} height="40px" width="40px" alt="shoe" />
+        </td>
+      </tr>
+    );
+  });
   return (
-    <div>
-      <div>
-        {cart.map((shoe, i) => {
-          return (
-            <table className="table-container1">
-              <thead>
-                <th>shoes Names</th>
-                <th>shoes size</th>
-                <th> shoes price</th>
-                <th>Shoes Image</th>
-              </thead>
-              <tbody>
-                <tr key={i} className="shoes-table">
-                  <td>{shoe.name}</td>
-                  <td>{shoe.size}</td>
-                  <td>{shoe.price}</td>
-                  <td>
-                    <img
-                      src={shoe.imgUrl}
-                      height="40px"
-                      width="40px"
-                      alt="shoe"
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          );
-        })}
-      </div>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr className="shoes-row">
+            <th>shoes Names</th>
+            <th>shoes size</th>
+            <th> shoes price</th>
+            <th>Shoes Image</th>
+          </tr>
+        </thead>
+
+        <tbody>{tablerows}</tbody>
+      </table>
     </div>
   );
 }
